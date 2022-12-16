35faaeac25f1dd6564171ff5020c60e8dc752251
Alex <zhestkov.82@mail.ru>
2022-12-16 14:07:13 +0400

    Examples003_SumRandom

--- Examples003_Sum/Program.cs
+++ Examples003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 21;
-int numberB = 25;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA+numberB;
 Console.WriteLine(result);
